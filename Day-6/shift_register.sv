ae1c5765e67e68f8cc23b03c5154cf6cc8c5803c
Rahul Kumar <rahulmac@Rahuls-MacBook-Air.local>
2025-11-03 14:20:30 +0530

    init day-6 shift_register

---
+++ Day-6/shift_register.sv
@@ -0,0 +1,17 @@
+// N-bit left shift register
+module shift_reg #(
+    parmeter WIDTH = 8
+) (
+    input logic clk, arst, inp,
+    output logic [$clog2(WIDTH)-1:0] out
+);
+    always @(posedge clk or posedge arst) begin
+        if (arst) out <= '0;
+        else out <= {out[WIDTH-2:0], inp}; // left shift
+        // else out <= {inp, out[WIDTH-1:1]}; // right shift
+
+        // else out <= {out[WIDTH-2:0], out[WIDTH-1]}; // rotate left shift
+
+    end
+endmodule
+
